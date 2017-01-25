6d65b08e7ed37147580276cac346c9dc22d95e9e
artem ovcharenko <akovcharenko"gmail.com>
2017-01-25 21:05:34 +0100

    navigation ready

---
+++ app/components/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-comix-app',
+  templateUrl: 'app/templates/app.component.html',
+  styleUrls: ['app/css/app.component.css']
+})
+export class AppComponent { }
---
+++ app/components/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mw-header',
+    templateUrl: 'app/templates/header.component.html',
+    styleUrls: ['app/css/header.component.css']
+})
+
+
+export class HeaderComponent { }
